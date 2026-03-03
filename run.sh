c893a9f5fa5266359258a72880e59b049cefa8ab
Ludovic Dutoit <dutoit.ludovic@gmail.com>
2026-03-03 16:05:03 +1300

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@
 module load Mamba
 eval "$(mamba shell hook --shell bash)"
 mamba activate /nesi/nobackup/uoo00116/conda/envs/snparcher
-snakemake -n !!  --profile /
+module load snakemake/7.32.3-foss-2023a-Python-3.11.6
+snakemake -n --profile workflow-profiles/default/
