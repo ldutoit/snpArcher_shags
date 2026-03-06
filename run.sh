9728bd2cf0de25df58e491fb59fbfb7f6098baff
Ludovic Dutoit <dutoit.ludovic@gmail.com>
2026-03-06 14:27:15 +1300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-module load Mamba
+module load Miniforge3
 eval "$(mamba shell hook --shell bash)"
 mamba activate /nesi/nobackup/uoo00116/conda/envs/snparcher
-module load snakemake/7.32.3-foss-2023a-Python-3.11.6
 snakemake -n --profile workflow-profiles/default/
