51f204d4391e0e196fe424913f392b88e0452942
Ludovic Dutoit <dutoit.ludovic@gmail.com>
2026-03-27 11:52:53 +1300

    Delete run.sh

--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-module load Miniforge3
-eval "$(mamba shell hook --shell bash)"
-mamba activate /nesi/nobackup/uoo00116/conda/envs/snparcher
-snakemake -n --profile workflow-profiles/default/
